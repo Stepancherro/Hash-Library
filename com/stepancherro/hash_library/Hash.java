5dcd1cab0f1880d720edf8a2670b2905c5984dfe
Stepancherro <48149292+Stepancherro@users.noreply.github.com>
2020-07-18 17:59:46 +0300

    Optimize code
    New hash function with the ability to add a type of hash

--- com/stepancherro/hash_library/Hash.java
+++ com/stepancherro/hash_library/Hash.java
@@ -3,10 +3,11 @@ package com.stepancherro.hash_library;
 import java.security.MessageDigest;
 
 public class Hash {
+    // allowed "SHA-256", "SHA-1", "SHA-384", "SHA-512", "MD5", "MD2"};
 
-    public static String sha256(String base) {
+    public static String hash(String type, String base) {
         try{
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            MessageDigest digest = MessageDigest.getInstance(type);
             byte[] hash = digest.digest(base.getBytes("UTF-8"));
             StringBuffer hexString = new StringBuffer();
 
@@ -17,98 +18,11 @@ public class Hash {
             }
 
             return hexString.toString();
-        } catch(Exception ex){
-            throw new RuntimeException(ex);
         }
-    }
+        catch (Exception e){
+            return "Error: " + e;
 
-    public static String sha1(String base) {
-        try{
-            MessageDigest digest = MessageDigest.getInstance("SHA-1");
-            byte[] hash = digest.digest(base.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
-
-            for (int i = 0; i < hash.length; i++) {
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch(Exception ex){
-            throw new RuntimeException(ex);
         }
-    }
 
-    public static String sha384(String base) {
-        try{
-            MessageDigest digest = MessageDigest.getInstance("SHA-384");
-            byte[] hash = digest.digest(base.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
-
-            for (int i = 0; i < hash.length; i++) {
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch(Exception ex){
-            throw new RuntimeException(ex);
-        }
-    }
-
-    public static String sha512(String base) {
-        try{
-            MessageDigest digest = MessageDigest.getInstance("SHA-512");
-            byte[] hash = digest.digest(base.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
-
-            for (int i = 0; i < hash.length; i++) {
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch(Exception ex){
-            throw new RuntimeException(ex);
-        }
-    }
-
-    public static String md5(String base) {
-        try{
-            MessageDigest digest = MessageDigest.getInstance("MD5");
-            byte[] hash = digest.digest(base.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
-
-            for (int i = 0; i < hash.length; i++) {
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch(Exception ex){
-            throw new RuntimeException(ex);
-        }
-    }
-
-    public static String md2(String base) {
-        try{
-            MessageDigest digest = MessageDigest.getInstance("MD2");
-            byte[] hash = digest.digest(base.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
-
-            for (int i = 0; i < hash.length; i++) {
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch(Exception ex){
-            throw new RuntimeException(ex);
-        }
     }
 }
